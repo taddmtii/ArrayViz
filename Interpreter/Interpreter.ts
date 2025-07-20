c397e1c879959c4fb6fa1b4939c86953fa52edf8
Tadd Trumbull II <taddtrumbullii@gmail.com>
2025-07-19 21:19:26 -0400

    laid ground work for beginning interpreter

---
+++ Interpreter/Interpreter.ts
@@ -0,0 +1,16 @@
+// References:
+// https://medium.com/@alessandro.traversi/understanding-the-command-design-pattern-in-typescript-1d2ee3615da8
+
+
+// Command: interface that tells us how to carry out an operation.
+interface Command {
+
+}
+
+// Invoker: component that uses command to carry out an operation.
+
+
+// Receiver: component that knows how to perform operations.
+class Interpreter {
+
+}
